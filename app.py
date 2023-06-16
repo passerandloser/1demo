37680b26bbfbd227ad3bf574993627d02977c4c1
your name <passerandloser@outlook.com>
2023-06-16 13:52:14 +0800

    代码下载

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from flask import send_from_directory, send_file
 
 app = Flask(__name__)
 
@@ -15,6 +16,11 @@ def index():
 @app.route('/')
 def default_route():
     return render_template('homepage.html')
+
+@app.route('/download')
+def download():
+    return send_file('./data/代码.txt',
+                )
 
 
 if __name__ == '__main__':
