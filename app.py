b4a5fe90772ee91118c276a96edd76e23a951560
your name <passerandloser@outlook.com>
2023-06-16 13:25:44 +0800

    代码

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def home():
 def index():
     return render_template('index.html')
 
+@app.route('/')
+def default_route():
+    return render_template('homepage.html')
+
 
 if __name__ == '__main__':
     app.run()
